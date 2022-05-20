2f2b786ef9bbc2863fb9bb4d90f5e853d4e475f2
Studiosfir3917 <AaronThomasJonesAj@outlook.com>
2022-05-19 19:32:23 -0600

    Created classes

--- family_quest_chart/character.py
+++ family_quest_chart/character.py
@@ -1,5 +1,26 @@
+"""Class for Character"""
+# Standard imports
+from uuid import uuid4
+
+# Third party imports
+
+# Local imports
+
+# Copyright Aaron Jones 2022
 
 
+class Character:
+    """Character are the player."""
+    def __init__(self):
+        """Initialize the character class"""
+        self.uuid = uuid4()
+        self.username = ''
+        self.first_name = ''
+        self.last_name = ''
+        self.email = ''
+        # self.birthday = ''
+        self.points = 0
+        self.inventory = []
 
 # username
 # first name
--- family_quest_chart/quest.py
+++ family_quest_chart/quest.py
@@ -1,5 +1,24 @@
+"""Class for Quest"""
+# Standard imports
+from uuid import uuid4
+
+# Third party imports
+
+# Local imports
+
+# Copyright Aaron Jones 2022
 
 
+class Quest:
+    """Quest is the chore."""
+    def __init__(self):
+        """Initialize the quest class."""
+        self.uuid = uuid4()
+        self.short_name = ''
+        self.description = ''
+        self.priority = 'essential'
+        self.recurrence = 'daily'
+        self.points = 0
 
 # uuid
 # short name
